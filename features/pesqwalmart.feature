bdfc1920e8df556be4b9760bee7687add26d348f
Santander <santander@MAC2152194.local>
2017-02-22 10:07:19 -0300

    upload de arquivos

---
+++ features/pesqwalmart.feature
@@ -0,0 +1,22 @@
+#language: pt
+#utf-8
+
+Funcionalidade: Validar Iphone 7
+Eu como cliente
+Quero analisar as novidades 
+Para me atualizar no mercado
+
+Cenario: Validar Iphone 7
+Dado que estou no site walmart
+Quando pesquiso por apple
+Então visualizo os produtos da apple
+
+Cenario: Validar Iphone 6s
+Dado que estou na pagina da apple
+Quando clico no Iphone 6 dourado
+Então vizualizo os dados para compra
+
+Cenario: Calcular o frete
+Dado que selecionei o iphone dourado 6s
+Quando clico em Calcular frete
+Então vizualizo os valores e dias possiveis
---
+++ features/step_definitions/pesqwalmart.rb
@@ -0,0 +1,60 @@
+Dado(/^que estou no site walmart$/) do
+  visit 'https://www.walmart.com.br'
+  sleep 5
+end
+
+Quando(/^pesquiso por apple$/) do
+	fill_in('ft', :with => 'apple')
+	find('li.item.autocomplete-suggestion', :text => 'celular apple').click
+	click_button('Procurar')
+	sleep 5
+ end
+
+Então(/^visualizo os produtos da apple$/) do
+  expect(page.has_content?('apple')).to eq true
+  sleep 5
+end
+
+Dado(/^que estou na pagina da apple$/) do
+  visit 'https://www.walmart.com.br'
+  fill_in('ft', :with => 'apple')
+	find('li.item.autocomplete-suggestion', :text => 'celular apple').click
+	click_button('Procurar')
+	 expect(page.has_content?('apple')).to eq true
+  sleep 5
+end
+
+Quando(/^clico no Iphone (\d+) dourado$/) do |arg1|
+	#find('iphone 6s Apple 32GB Dourado MN112BR/A', match: :first).click
+	click_link('iPhone 6s Apple 32GB Dourado MN112BR/A', match: :first)
+  sleep 5
+end
+
+Então(/^vizualizo os dados para compra$/) do
+  has_content?('Formas de pagamento?')
+
+sleep 5
+end
+
+Dado(/^que selecionei o iphone dourado (\d+)s$/) do |arg1|
+  visit 'https://www.walmart.com.br'
+  fill_in('ft', :with => 'apple')
+  find('li.item.autocomplete-suggestion', :text => 'celular apple').click
+  click_button('Procurar')
+  expect(page.has_content?('apple')).to eq true
+  click_link('iPhone 6s Apple 32GB Dourado MN112BR/A', match: :first)
+  # has_content?('Formas de pagamento?')
+end
+
+Quando(/^clico em Calcular frete$/) do
+   fill_in('estimate-shipping-txt-cep', :with => '04943080')
+   click_button('Calcular frete')
+   sleep 10
+end
+
+Então(/^vizualizo os valores e dias possiveis$/) do
+  assert_text('Opções de frete para sua região')
+  sleep 5
+end
+
+
